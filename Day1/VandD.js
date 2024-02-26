cc0a3ac8c708a661271e797bf4f69b07caa9bed1
Ialbertine <a.ingabire1@alustudent.com>
2024-02-26 12:25:38 +0200

    javascript Variable and Datatype

---
+++ Day1/VandD.js
@@ -0,0 +1,13 @@
+const myname = 'Albertine';
+const district = 'Gasabo';
+const issheaboy = false;
+console.log("My name is " +myname+ " and i live at " +district+ " District");
+console.log(`my name is  ${myname} and i live at ${district} district`);
+
+//array
+
+const array = ['string' , 1, 2, false]
+const myfavorite = ['ubugari', 'eggs', 'meat'];
+
+console.log(array);
+console.log(myfavorite)
