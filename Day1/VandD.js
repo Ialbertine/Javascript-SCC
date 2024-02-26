49434d48b78115856f752b3e12ef9810be6a8aaf
Ialbertine <a.ingabire1@alustudent.com>
2024-02-26 12:50:59 +0200

    javascript Variable and Datatype

--- Day1/VandD.js
+++ Day1/VandD.js
@@ -11,3 +11,29 @@ const myfavorite = ['ubugari', 'eggs', 'meat'];
 
 console.log(array);
 console.log(myfavorite)
+//functions 
+function mylastname(){
+    let last_name = 'Ingabire';
+    console.log(last_name);
+}
+mylastname();
+last_name= 'Tina';
+console.log(last_name);
+
+//object is a set of key pair value
+//declaration and definition
+
+
+let frontendclass = {
+    team1:{
+        member: 12,
+        gender: 'female',
+        single: false,
+    },
+    team2:{
+        member: 13,
+        gender: 'male',
+        single: true,
+    }
+}
+console.log(frontendclass);
